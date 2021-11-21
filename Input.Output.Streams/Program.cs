fb6df9c602d5c16d36ef5dbf9b31f1c870786c13
Paula Morais <pmorais78@gmail.com>
2021-11-21 01:16:29 -0300

    AddConvertFileToCurrentAccount method

--- Input.Output.Streams/Program.cs
+++ Input.Output.Streams/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Input.Output.Streams.Constants;
+using Input.Output.Streams.Models;
 using System;
 using System.IO;
+using System.Text;
 
 namespace Input.Output.Streams
 {
@@ -12,17 +14,42 @@ namespace Input.Output.Streams
 
             using (var fileStream = new FileStream(file, FileMode.Open))
 
-            using (var reader = new StreamReader(fileStream))
+            using (var reader = new StreamReader(fileStream, Encoding.UTF8))
             {
                 while (!reader.EndOfStream)
                 {
                     string line = reader.ReadLine();
-                    Console.WriteLine(line);
-                }
+                    var currentAccount = ConvertFileToCurrentAccount(line);
+                    var mesage = $"{currentAccount.Holder.Name} : Conta número { currentAccount.NumberAccount}, ag. { currentAccount.Branch}, Saldo { currentAccount.Balance}";
 
+                    Console.WriteLine(mesage);
+                }
             }
 
             Console.ReadLine();
+        }
+
+        static CurrentAccount ConvertFileToCurrentAccount(string line)
+        {
+            var fields = line.Split(' ');
+
+            var branch = fields[0];
+            var numberAccount = fields[1];
+            var balance = fields[2];
+            var nameHolder = fields[3];
+
+            var parsedBranch = int.Parse(branch);
+            var parsedNumberAccount = int.Parse(numberAccount);
+            var parsedBalance = double.Parse(balance.Replace('.', ','));
+
+            var holder = new Client
+            {
+                Name = nameHolder
+            };
+            var result = new CurrentAccount(parsedBranch, parsedNumberAccount);
+            result.Deposit(parsedBalance);
+            result.Holder = holder;
+            return result;
         }
     }
 }
