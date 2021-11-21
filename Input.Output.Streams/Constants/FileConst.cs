b20140aeba74bb3efff2fb22bd364bbd9a71326a
Paula Morais <pmorais78@gmail.com>
2021-11-21 01:47:45 -0300

    Added csv file with stream writer

--- Input.Output.Streams/Constants/FileConst.cs
+++ Input.Output.Streams/Constants/FileConst.cs
@@ -3,5 +3,6 @@
     public static class FileConst
     {
         public const string FilePath = @"D:\Códigos\input-output-streams-csharp\accounts.txt";
+        public const string FilePathCsv = @"D:\Códigos\input-output-streams-csharp\accounts.csv";
     }
 }
--- Input.Output.Streams/CreateCsvFile.cs
+++ Input.Output.Streams/CreateCsvFile.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Input.Output.Streams.Constants;
+using System.IO;
 using System.Text;
 
 namespace Input.Output.Streams
 {
-    class CreateCsvFile
+    partial class Program
     {
+        static void CreateFile()
+        {
+            var fileCsv = FileConst.FilePathCsv;
+
+            using var fileStream = new FileStream(fileCsv, FileMode.Create);
+            using var writer = new StreamWriter(fileStream, Encoding.UTF8);
+            writer.WriteLine("456,65465,456.0,Pedro");
+        }
     }
 }
