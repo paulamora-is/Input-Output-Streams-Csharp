a76a4cfacc3aa088fc32a193e6cf303a70fcab6b
Paula Morais <pmorais78@gmail.com>
2021-11-22 23:06:24 -0300

    Add FileClassAssistants

--- Input.Output.Streams/Constants/FileConst.cs
+++ Input.Output.Streams/Constants/FileConst.cs
@@ -2,10 +2,11 @@
 {
     public static class FileConst
     {
-        public const string FilePath = @"D:\Códigos\input-output-streams-csharp\accounts.txt";
+        public const string FilePathAccounts = @"D:\Códigos\input-output-streams-csharp\accounts.txt";
         public const string FilePathWithFlushMethod = @"D:\Códigos\input-output-streams-csharp\accountsWithFlush.txt";
         public const string FilePathCsv = @"D:\Códigos\input-output-streams-csharp\accounts.csv";
         public const string FilePathBinary = @"D:\Códigos\input-output-streams-csharp\fileBinary.txt";
         public const string FilePathConsoleBytes = @"D:\Códigos\input-output-streams-csharp\consolebytes.txt";
+        public const string FilePathFileClassHelpers = @"D:\Códigos\input-output-streams-csharp\fileClassHelpers.txt";
     }
 }
---
+++ Input.Output.Streams/FileClassAssistants.cs
@@ -0,0 +1,21 @@
+﻿using Input.Output.Streams.Constants;
+using System;
+using System.IO;
+
+namespace Input.Output.Streams
+{
+    partial class Program
+    {
+        static void UseFileClassHelpers()
+        {
+            File.WriteAllText(FileConst.FilePathFileClassHelpers, "Testing File.WriteAllText");
+            Console.WriteLine("File criated!");
+
+            var byteFile = File.ReadAllBytes(FileConst.FilePathAccounts);
+            Console.WriteLine($"File account.txt has {byteFile.Length} bytes");
+
+            var lines = File.ReadAllLines(FileConst.FilePathAccounts);
+            Console.WriteLine($"The file account.txt has {lines.Length} lines");
+        }
+    }
+}
