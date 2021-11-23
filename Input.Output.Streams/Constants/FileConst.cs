d6033d05761468270f15e23e41e116a89dd98734
Paula Morais <pmorais78@gmail.com>
2021-11-22 21:50:09 -0300

    Add FileWithFlush

--- Input.Output.Streams/Constants/FileConst.cs
+++ Input.Output.Streams/Constants/FileConst.cs
@@ -3,6 +3,7 @@
     public static class FileConst
     {
         public const string FilePath = @"D:\Códigos\input-output-streams-csharp\accounts.txt";
+        public const string FilePathWithFlushMethod = @"D:\Códigos\input-output-streams-csharp\accountsWithFlush.txt";
         public const string FilePathCsv = @"D:\Códigos\input-output-streams-csharp\accounts.csv";
     }
 }
---
+++ Input.Output.Streams/FileWithFlush.cs
@@ -0,0 +1,26 @@
+﻿using Input.Output.Streams.Constants;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Input.Output.Streams
+{
+    partial class Program
+    {
+        static void CreateFileWithFlushMethod()
+        {
+            var fileCsv = FileConst.FilePathWithFlushMethod;
+
+            using var fileStream = new FileStream(fileCsv, FileMode.Create);
+            using var writer = new StreamWriter(fileStream, Encoding.UTF8);
+
+            for (var i = 0; i < 10000; i++)
+            {
+                writer.WriteLine($"This is line number {i}");
+                writer.Flush();
+                Console.WriteLine($"Line {i} was written to file. Hit 'enter' to add one more");
+                Console.ReadLine();
+            }
+        }
+    }
+}
